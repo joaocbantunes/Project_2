36c6255e5991602c7860c0bb13d1e47cc32951bc
João Antunes <joaocbantunes@gmail.com>
2022-05-13 16:24:31 +0100

    artist result deleted

--- routes/artist.routes.js
+++ routes/artist.routes.js
@@ -1,17 +1,26 @@
 //const discogsWebAPI = require("https://api.discogs.com");
 const router = require("express").Router();
-
+/* 
 router.get("/artist-search", (req, res, next) => {
   discogsAPI
     .searchArtists(req.query.name)
     .then((data) => {
       const findArtists = data.body.artists.items;
       console.log(findArtists);
-      res.render("artist-search-results", { findArtists });
+      res.render("artist-search", { findArtists });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
+}); */
+
+router.get("/artist-search", (req, res, next) => {
+  res.render("artist-search");
 });
+/* .catch((err) =>
+    console.log("The error while searching artists occurred: ", err)
+  );
+ */
+
 
 module.exports = router;
