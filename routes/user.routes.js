0a72ad163d5a1621eb290f6a2533b4c766e5c20b
JoaoMorgado8 <joaoarmorgado@gmail.com>
2022-05-16 12:51:55 +0100

    adds collection

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,16 @@ const axios = require("axios");
 const User = require("../models/User.model");
 const router = require("express").Router();
 
-router.get("/user", (req, res) => {
+router.get("/user", (req, res, next) => {
   res.render("profile/user");
+});
+
+router.get("/user/collection", (req, res, next) => {
+  res.render("profile/collection");
+});
+
+router.get("/user/wishlist", (req, res, next) => {
+  res.render("profile/wishlist");
 });
 
 module.exports = router;
